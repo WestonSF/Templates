4e5e30b63b4360c814778e1fee8788b79c173ca2
Shaun Weston <shaun_weston@eagle.co.nz>
2013-11-22 09:54:57 +1300

    Adding of example for python template

---
+++ PythonTemplateExample.bat
@@ -0,0 +1,7 @@
+REM ----- Python template example -----
+c:\python27\arcgis10.2\python "C:\Development\Development Templates\PythonTemplateExample.py" ^
+ "C:\Downloads\New File Geodatabase.gdb\WairarapaWards" ^
+ "C:\Downloads\New File Geodatabase.gdb\WairarapaWards\Output" ^
+ "40"
+
+pause
---
+++ PythonTemplateExample.py
@@ -0,0 +1,123 @@
+#-------------------------------------------------------------
+# Name:       Python Template Example
+# Purpose:    Buffer a feature class by a specified amount.     
+# Author:     Shaun Weston (shaun_weston@eagle.co.nz)
+# Date Created:    01/01/2013
+# Last Updated:    01/01/2013
+# Copyright:   (c) Eagle Technology
+# ArcGIS Version:   10.1/10.2
+# Python Version:   2.7
+#--------------------------------
+
+# Import modules and enable data to be overwritten
+import os
+import sys
+import datetime
+import smtplib
+import arcpy
+arcpy.env.overwriteOutput = True
+
+# Set variables
+logInfo = "true"
+logFile = r"C:\Development\Development Templates\PythonTemplateExample.log"
+sendEmail = "true"
+emailTo = "shaun_weston@eagle.co.nz"
+emailUser = "mdcgisserver@gmail.com"
+emailPassword = "Spl1ceGroup"
+emailSubject = "GIS Error"
+emailMessage = "There was an error..."
+output = None
+
+# Start of main function
+def mainFunction(inputFeatureClass,output,bufferAmount): # Get parameters from ArcGIS Desktop tool by seperating by comma e.g. (var1 is 1st parameter,var2 is 2nd parameter,var3 is 3rd parameter)  
+    try:
+        # Log start
+        if logInfo == "true":
+            loggingFunction(logFile,"start","")
+
+        # --------------------------------------- Start of code --------------------------------------- #        
+
+        arcpy.Buffer_analysis(inputFeatureClass, output, bufferAmount + " Meters", "FULL", "ROUND", "NONE", "")
+        
+        # --------------------------------------- End of code --------------------------------------- #  
+            
+        # If called from gp tool return the arcpy parameter   
+        if __name__ == '__main__':
+            # Return the output if there is any
+            if output:
+                arcpy.SetParameterAsText(1, output)
+        # Otherwise return the result          
+        else:
+            # Return the output if there is any
+            if output:
+                return output      
+        # Log end
+        if logInfo == "true":
+            loggingFunction(logFile,"end","")        
+        pass
+    # If arcpy error
+    except arcpy.ExecuteError:
+        # Show the message
+        arcpy.AddMessage(arcpy.GetMessages(2))        
+        # Log error
+        if logInfo == "true":  
+            loggingFunction(logFile,"error",arcpy.GetMessages(2))
+    # If python error
+    except Exception as e:
+        # Show the message
+        arcpy.AddMessage(e.args[0])          
+        # Log error
+        if logInfo == "true":         
+            loggingFunction(logFile,"error",e.args[0])
+# End of main function
+
+# Start of logging function
+def loggingFunction(logFile,result,info):
+    #Get the time/date
+    setDateTime = datetime.datetime.now()
+    currentDateTime = setDateTime.strftime("%d/%m/%Y - %H:%M:%S")
+    
+    # Open log file to log message and time/date
+    if result == "start":
+        with open(logFile, "a") as f:
+            f.write("---" + "\n" + "Process started at " + currentDateTime)
+    if result == "end":
+        with open(logFile, "a") as f:
+            f.write("\n" + "Process ended at " + currentDateTime + "\n")
+            f.write("---" + "\n")
+    if result == "warning":
+        with open(logFile, "a") as f:
+            f.write("\n" + "Warning: " + info)               
+    if result == "error":
+        with open(logFile, "a") as f:
+            f.write("\n" + "Process ended at " + currentDateTime + "\n")
+            f.write("Error: " + info + "\n")        
+            f.write("---" + "\n")
+        # Send an email
+        if sendEmail == "true":
+            arcpy.AddMessage("Sending email...")
+            # Server and port information
+            smtpserver = smtplib.SMTP("smtp.gmail.com",587) 
+            smtpserver.ehlo()
+            smtpserver.starttls() 
+            smtpserver.ehlo
+            # Login with sender email address and password
+            smtpserver.login(emailUser, emailPassword)
+            # Email content
+            header = 'To:' + emailTo + '\n' + 'From: ' + emailUser + '\n' + 'Subject:' + emailSubject + '\n'
+            message = header + '\n' + emailMessage + '\n' + '\n' + info
+            # Send the email and close the connection
+            smtpserver.sendmail(emailUser, emailTo, message)
+            smtpserver.close()                
+# End of logging function    
+
+# This test allows the script to be used from the operating
+# system command prompt (stand-alone), in a Python IDE, 
+# as a geoprocessing script tool, or as a module imported in
+# another script
+if __name__ == '__main__':
+    # Arguments are optional - If running from ArcGIS Desktop tool, parameters will be loaded into *argv
+    argv = tuple(arcpy.GetParameterAsText(i)
+        for i in range(arcpy.GetArgumentCount()))
+    mainFunction(*argv)
+    
